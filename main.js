71a3c5cd3261900319dbdae4f6295bb966a3e111
Giuliano Butera <gbutera140@gmail.com>
2024-10-01 21:47:19 -0300

    Segunda Pre entrega

--- main.js
+++ main.js
@@ -1,210 +1,229 @@
 /**
- * La funcionalidad de la app es poder almacenar stock de cualquier cosa, ya sea una categoría que tenga distintos ítems (ej: frutas),
- * o bien ítems simples que no se pueda (o no se quiera) almacenar en una categoría (ej: figuritas repetidas de un álbum).
- * 
- * Hay ciertas partes del código que pueden ser avanzadas, como los comentarios (no descarto tener errores por supuesto), es porque trabajo como dev web front hace 5 años, 
- * especificamente con angular, estoy haciendo la carrera de full stack por eso estoy en el curso, además que me gusta repasar las bases nuevamente y sobre todo
- * aprender bien js vanilla porque normalmente trabajo con typescript. :)
+ * Por cuestiones de tiempo no llegué a hacer la carga de stock de categorías,
+ * pero para cumplir con la pre entrega, queda lista la carga de stock de ítems simples.
+ * de todas maneras dejo un array a modo de prueba para revisar cómo se vería
+ * un stock de categorías (en la prox pre entrega agrego esa funcionalidad)
  */
+
+/**
+ * Clase para inicializar stock
+ */
+class Stock {
+    constructor(id, nombreStock, itemSimple, descripcion, items, cantidad) {
+        this.id = id;
+        this.nombreStock = nombreStock;
+        this.itemSimple = itemSimple;
+        this.descripcion = descripcion;
+        this.items = items ?? null;
+        this.cantidad = cantidad ?? null;
+    }
+}
 
 /**
  * Array que permite manejar y almacenar el stock.
  */
-const manejadorStock = [];
+let manejadorStock = [];
+// Pruebas de categorías
+// let manejadorStock = [
+//     {
+//         id: 1,
+//         nombreStock: "Frutas",
+//         itemSimple: false,
+//         descripcion: "Stock de frutas en mi depto",
+//         items: [{idItem: 1, nombre: "Banana", cantidad: 4}, {idItem: 2, nombre: "Manzana", cantidad: 2}, {idItem: 3, nombre: "Mandarina", cantidad: 2}],
+//         cantidad: null
+//     },
+//     {
+//         id: 2,
+//         nombreStock: "Verduras",
+//         itemSimple: false,
+//         descripcion: "Stock de verduras en mi depto",
+//         items: [{idItem: 1, nombre: "Cebolla", cantidad: 2}, {idItem: 2, nombre: "Tomate", cantidad: 4}],
+//         cantidad: null
+//     }
+// ];
 
-/**
- * Objeto que contiene una enumeración de todas las opciones disponibles en el menú.
- */
-const enumOpcionesMenu = {
-    retornarAMenu: 0,
-    mostrarStock: 1,
-    agregarStock: 2,
-    editarStock: 3,
-    salir: 4
-}
-
-/**
- * Objeto que contiene una enumeración de todas los tipos de stock.
- */
-const enumOpcionesTipoStock = {
-    categoria: 1,
-    itemSimple: 2,
-    volverAMenuPrincipal: 3
-}
-
-/**
- * Función para mostrar el stock cargado hasta el momento.
- */
-const mostrarStock = () => {
-    if (manejadorStock.length == 0) {
-        return alert("Aún no ha agregado ninguna categoría o ítem simple al stock. \nPara agregar, seleccione la opción número 2 en el menú principal.");
-    }
-    let mensajeAlert = "Categorías e ítems simples ingresados hasta el momento: \n\n";
-    manejadorStock.forEach(stock => {
-        if (stock.itemSimple) {
-            mensajeAlert = mensajeAlert +  `Nombre: ${stock.nombre}, Tipo: Ítem simple, Cantidad: ${stock.cantidad}\n\n`;
-        } else {
-            mensajeAlert = mensajeAlert + `Nombre: ${stock.nombre}, Tipo: Categoría, Ítems:\n`;
-            stock.items.forEach(item => {
-                mensajeAlert = mensajeAlert + `\tNombre: ${item.nombre}, Cantidad: ${item.cantidad}\n`;
-            });
-            mensajeAlert = mensajeAlert + "\n"
-        }
+const renderizarFormularioNuevoStock = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const form = document.getElementById('formularioNuevoStock');
+        form.addEventListener('submit', (event) => {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                event.preventDefault();
+                const nuevoStock = new Stock(
+                    Math.floor(Math.random() * 1001),
+                    document.getElementById('nombreStock').value,
+                    true,
+                    document.getElementById('descripcion').value ?? '',
+                    null,
+                    parseInt(document.getElementById('cantidad').value)
+                );
+                manejadorStock.push(nuevoStock);
+                guardarStockLocal();
+                renderizarStock();
+                $('#modalNuevoStock').modal('hide');
+            }
+            form.classList.add('was-validated');
+        }, false);
+        
+        // Reiniciar el formulario cuando se abre el modal
+        $('#modalNuevoStock').on('show.bs.modal', function () {
+            form.reset();
+            form.classList.remove('was-validated');
+        });
     });
-    return alert(`${mensajeAlert}`);
 }
 
 /**
- * Función para agregar nuevas categorías o ítems simples al manejador de stock.
- * @param {string} nombreStock Nombre de la categoría o ítem a agregar.
- * @param {boolean} itemSimple Si el valor es true es un ítem simple, si es false es una categoría.
- * @param {Array<{nombre: string, cantidad: number}>} [items=null] Contiene nombre y cantidad de los ítems, si es un ítem simple no será utilizado.
- * @param {number} [cantidad=null] Se utiliza para representar la cantidad que tendrá un ítem simple, si es una categoría no se utilizará.
+ * Función para obtener de localStorage el stock guardado
  */
-const agregarStock = (nombreStock, itemSimple, items, cantidad) => {
-    let nuevoStock = {
-        nombre: nombreStock,
-        itemSimple: itemSimple,
-    }
-    if (itemSimple) {
-        nuevoStock.cantidad = cantidad;
+const obtenerStockLocal = () => {
+    manejadorStock = JSON.parse(localStorage.getItem("manejadorStock")) || [];
+};
+
+/**
+ * Función para guardar en localStorage el stock editado
+ */
+const guardarStockLocal = () => {
+    localStorage.setItem('manejadorStock', JSON.stringify(manejadorStock));
+};
+
+/**
+ * Función para renderizar en html el manejador de stock
+ */
+const renderizarStock = () => {
+    if (manejadorStock.length > 0) {
+        // Obtener y ocultar empty state
+        const emptyStateStock = document.getElementById("emptyStateStock");
+        emptyStateStock.classList.add("d-none");
+        // Obtener contenedor de cards y agregar stock
+        const cardsStock = document.getElementById("cardsStock");
+        cardsStock.innerHTML="";
+        manejadorStock.forEach(({id, nombreStock, itemSimple, descripcion, items, cantidad}) => {
+            let nuevaCard = document.createElement("div");
+            nuevaCard.classList.add("col-sm-6", "col-md-4", "col-lg-3", "mb-4");
+            nuevaCard.id = `cardStock${id}`;
+            nuevaCard.innerHTML = `
+                <div class="card bg-primary text-white">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-2">
+                            <div class="d-flex align-items-center">
+                                <h5 class="card-title mb-0">
+                                    ${nombreStock}
+                                    ${itemSimple ? '' : '<i class="bi bi-folder-fill icon-margin-left" title="Categoría"></i>'}
+                                </h5>
+                            </div>
+                            <button class="btn btn-outline-light ml-2 btn-sm" type="button" id="eliminarStock${id}">
+                                <i class="bi bi-trash3-fill"></i>
+                            </button>
+                        </div>
+                        <p class="card-text">${descripcion}</p>
+                        ${itemSimple 
+                        ? `
+                            <div class="d-flex align-items-center justify-content-end">
+                                <p class="card-text mb-0 custom-card-text">Cantidad:</p>
+                                <button class="btn btn-outline-light mr-2 btn-sm" type="button" id="restarStockItemSimple${id}">
+                                    <i class="bi bi-dash-lg"></i>
+                                </button>
+                                <span class="mx-2">${cantidad}</span>
+                                <button class="btn btn-outline-light ml-2 btn-sm" type="button" id="sumarStockItemSimple${id}">
+                                    <i class="bi bi-plus-lg"></i>
+                                </button>
+                            </div>
+                        ` 
+                        : `
+                            <p class="card-text mb-0">Ítems:</p>
+                            <ul class="list-group list-group-flush" id="listaItemsCategoria${id}">
+                            </ul>
+                        `}
+                    </div>
+                </div>
+            `;
+            cardsStock.appendChild(nuevaCard);
+            // TODO: Agregar validación para preguntar si desea eliminar
+            const botonEliminar = document.getElementById(`eliminarStock${id}`);
+            botonEliminar.addEventListener("click", () => {
+                manejadorStock = manejadorStock.filter(stock => {
+                    if (stock.id !== id) {
+                        return stock
+                    }
+                });
+                guardarStockLocal();
+                renderizarStock();
+            });
+            if (itemSimple) {
+                // Evento de suma y resta de stock
+                const botonRestar = document.getElementById(`restarStockItemSimple${id}`);
+                if (manejadorStock.find(stock => stock.id == id).cantidad == 0) {
+                    botonRestar.classList.add("disabled");
+                }
+                botonRestar.addEventListener("click", () => {
+                    if (manejadorStock.find(stock => stock.id == id).cantidad > 0) {
+                        manejadorStock.find(stock => stock.id == id).cantidad -= 1;
+                        guardarStockLocal();
+                        renderizarStock();
+                    }
+                });
+                const botonSumar = document.getElementById(`sumarStockItemSimple${id}`);
+                botonSumar.addEventListener("click", () => {
+                    botonRestar.classList.remove("disabled");
+                    manejadorStock.find(stock => stock.id == id).cantidad += 1;
+                    guardarStockLocal();
+                    renderizarStock();
+                });
+            } else {
+                // Obtener contenedor de ítems de la categoría y añadir ítems
+                const listaItemsCategoria = document.getElementById(`listaItemsCategoria${id}`);
+                listaItemsCategoria.innerHTML = "";
+                items.forEach(({idItem, nombre, cantidad}) => {
+                    let nuevoItem = document.createElement("li");
+                    nuevoItem.classList.add("list-group-item", "custom-list-group-item", "d-flex", "align-items-center", "justify-content-between");
+                    nuevoItem.id = `item${idItem}`;
+                    nuevoItem.innerHTML = `
+                        ${nombre}
+                        <div class="d-flex align-items-center justify-content-end">
+                            <button class="btn btn-outline-light mr-2 btn-sm" type="button" id="restarStockCategoria${id}Item${idItem}">
+                                <i class="bi bi-dash-lg"></i>
+                            </button>
+                            <span class="mx-2">${cantidad}</span>
+                            <button class="btn btn-outline-light ml-2 btn-sm" type="button" id="sumarStockCategoria${id}Item${idItem}">
+                                <i class="bi bi-plus-lg"></i>
+                            </button>
+                        </div>
+                    `;
+                    listaItemsCategoria.appendChild(nuevoItem);
+                    const botonRestar = document.getElementById(`restarStockCategoria${id}Item${idItem}`);
+                    if (manejadorStock.find(stock => stock.id == id).items.find(item => item.idItem == idItem).cantidad == 0) {
+                        botonRestar.classList.add("disabled");
+                    }
+                    botonRestar.addEventListener("click", () => {
+                        if (manejadorStock.find(stock => stock.id == id).items.find(item => item.idItem == idItem).cantidad > 0) {
+                            manejadorStock.find(stock => stock.id == id).items.find(item => item.idItem == idItem).cantidad -= 1;
+                            guardarStockLocal();
+                            renderizarStock();
+                        }
+                    });
+                    const botonSumar = document.getElementById(`sumarStockCategoria${id}Item${idItem}`);
+                    botonSumar.addEventListener("click", () => {
+                        botonRestar.classList.remove("disabled");
+                        manejadorStock.find(stock => stock.id == id).items.find(item => item.idItem == idItem).cantidad += 1;
+                        guardarStockLocal();
+                        renderizarStock();
+                    });
+                });
+            }
+        });
     } else {
-        nuevoStock.items = items;
-    }
-    manejadorStock.push(nuevoStock);
-}
-
-/**
- * Función para generar los ítems de una categoría.
- * @param {string} nombreCategoria Indica el nombre que va a contener la categoría que abarque a los ítems a ingresar.
- * @returns {Array<{nombre: string, cantidad: number}>} Ítems que contiene la categoría.
- */
-const agregarItemsCategroria = (nombreCategoria) => {
-    let itemsCategoria = [];
-    let loopAgregarItems = true;
-    while (loopAgregarItems) {
-        let nombreItem = prompt(`Ingrese el nombre de un nuevo ítem para la categoría ${nombreCategoria}.`);
-        while (!nombreItem || nombreItem.trim() == "") {
-            nombreItem = prompt(`El nombre ingresado no puede ser vacío. \nIngrese el nombre de un nuevo ítem para la categoría ${nombreCategoria}.`);
-        }
-
-        let cantidadItem = parseInt(prompt(`Ingrese la cantidad de stock para el ítem ${nombreItem}`));
-        while (!cantidadItem || isNaN(cantidadItem)) {
-            cantidadItem = parseInt(prompt(`La cantidad ingresada debe ser un número. \nIngrese la cantidad de stock para el ítem ${nombreItem}`));
-        }
-
-        itemsCategoria.push({nombre: nombreItem, cantidad: cantidadItem});
-        if (!confirm("¿Desea agregar otro ítem?")) {
-            loopAgregarItems = false;
-        }
-    }
-    return itemsCategoria;
-}
-
-/**
- * Función para recolectar la información para agregar una nueva categoría o ítem simple al manejador de stock.
- */
-const agregarNuevoStock = () => {
-    let opSeleccionadaStock = parseInt(prompt("¿Qué tipo de stock desea agregar?: \n\n1 - Categoría \n2 - Ítem simple \n3 - Salir \n\nIngrese el número del tipo de stock a agregar."));
-
-    switch (opSeleccionadaStock) {
-        case enumOpcionesTipoStock.categoria:
-            let nombreCategoria = prompt("Ingrese el nombre de la nueva categoría.");
-            while (!nombreCategoria || nombreCategoria.trim() == '') {
-                nombreCategoria = prompt("El nombre ingresado no puede ser vacío. \nIngrese el nombre de la nueva categoría.");
-            }
-            let items = agregarItemsCategroria(nombreCategoria);
-            agregarStock(nombreCategoria, false, items, null);
-            break;
-        
-        case enumOpcionesTipoStock.itemSimple:
-            let nombreItemSimple = prompt("Ingrese el nombre del nuevo ítem simple.");
-            while (!nombreItemSimple || nombreItemSimple.trim() == '') {
-                nombreItemSimple = prompt("El nombre ingresado no puede ser vacío. \nIngrese el nombre del nuevo ítem simple.");
-            }
-            let cantidadItem = parseInt(prompt(`Ingrese la cantidad de stock para el ítem ${nombreItemSimple}`));
-            while (!cantidadItem || isNaN(cantidadItem)) {
-                cantidadItem = parseInt(prompt(`La cantidad ingresada debe ser un número. \nIngrese la cantidad de stock para el ítem ${nombreItemSimple}`));
-            }
-            agregarStock(nombreItemSimple, true, null, cantidadItem);
-            break;
-        
-        case enumOpcionesTipoStock.volverAMenuPrincipal:
-            break;
-        
-        default:
-            alert("La opción seleccionada no es válida, intente nuevamente.");
-            agregarNuevoStock();
+        emptyStateStock.classList.remove("d-none");
     }
 }
 
-/**
- * Función para editar categorías o ítems que ya fueron agregados.
- */
-const editarStock = () => {
-    return alert("Esta opción estará disponible pronto, estamos trabajando para usted :)");
-}
-
-/**
- * Función que hace las veces de menú inicial y al cual se vuelve luego de finalizar cualquier acción.
- * @returns {number} La opción seleccionada por el usuario.
- */
-const menuPrincipal = () => {
-    let opSeleccionada = prompt("¿Qué desea realizar?: \n\n1 - Mostrar stock existente \n2 - Agregar nuevo stock \n3 - Editar stock existente \n4 - Salir \n\nIngrese el número de la acción a realizar.");
-    return parseInt(opSeleccionada);
-}
-
-/**
- * Función para procesar la opción seleccionada en el menú principal.
- * @param {number} opSeleccionada Opción que se ha seleccionado en el menú.
- * @returns {number} Opción que se ha seleccionado en el menú.
- */
-const selectorMenu = (opSeleccionada) => {
-    switch (opSeleccionada) {
-        case enumOpcionesMenu.mostrarStock:
-            mostrarStock();
-            break;
-        
-        case enumOpcionesMenu.agregarStock:
-            agregarNuevoStock();
-            break;
-        
-        case enumOpcionesMenu.editarStock:
-            editarStock();
-            break;
-
-        case enumOpcionesMenu.salir:
-            break;
-        
-        default:
-            opSeleccionada = enumOpcionesMenu.retornarAMenu;
-            alert("La opción seleccionada no es válida, intente nuevamente.");
-    }
-    return opSeleccionada;
-}
-
-/**
- * Función para inicializar la app.
- */
 const app = () => {
-    alert("¡Bienvenido a MyStockFlow!");
-    let loopMenu = true;
-    do {
-        let opSeleccionada = selectorMenu(menuPrincipal());
-        switch (opSeleccionada) {
-            case enumOpcionesMenu.retornarAMenu:
-                loopMenu = true;
-                break;
-
-            case enumOpcionesMenu.salir:
-                loopMenu = !confirm("¿Está seguro que desea salir?");
-                break;
-        
-            default:
-                loopMenu = confirm("¿Desea realizar otra acción?");
-                break;
-        }
-    } while (loopMenu);
+    renderizarFormularioNuevoStock();
+    obtenerStockLocal();
+    renderizarStock();
 }
 
 app();
